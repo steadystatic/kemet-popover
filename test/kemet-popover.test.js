129d9fac93c7d320ff4889fb42a1703ba872f451
Hasani Rogers <hasani.rogers@gmail.com>
2020-12-23 16:51:03 -0500

    added unit tests

--- test/kemet-popover.test.js
+++ test/kemet-popover.test.js
@@ -3,37 +3,51 @@ import { html, fixture, expect } from '@open-wc/testing';
 import '../kemet-popover.js';
 
 describe('KemetPopover', () => {
-  it('has a default title "Hey there" and counter 5', async () => {
-    const el = await fixture(html`
+  it('has the correct defaults', async () => {
+    const element = await fixture(html`
       <kemet-popover></kemet-popover>
     `);
 
-    expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
+    expect(element.opened).to.equal(false);
+    expect(element.effect).to.equal(null);
+    expect(element.position).to.equal('top');
+    expect(element.tooltip).to.equal(false);
+    expect(element.customTooltip).to.equal(false);
+    expect(element.fireOnClick).to.equal(false);
+    expect(element.fireOnHover).to.equal(true);
   });
 
-  it('increases the counter on button click', async () => {
-    const el = await fixture(html`
-      <kemet-popover></kemet-popover>
+  it('displays a tooltip when specified', async () => {
+    const element = await fixture(html`
+      <kemet-popover tooltip></kemet-popover>
     `);
-    el.shadowRoot.querySelector('button').click();
 
-    expect(el.counter).to.equal(6);
+    expect(element.shadowRoot.getElementById('tooltip').getAttribute('id')).to.equal('tooltip');
   });
 
-  it('can override the title via attribute', async () => {
-    const el = await fixture(html`
-      <kemet-popover title="attribute title"></kemet-popover>
+  it('displays a custom tooltip when specified', async () => {
+    const element = await fixture(html`
+      <kemet-popover custom-tooltip></kemet-popover>
     `);
 
-    expect(el.title).to.equal('attribute title');
+    expect(element.shadowRoot.getElementById('custom-tooltip').getAttribute('id')).to.equal('custom-tooltip');
+  });
+
+  it('opens the tooltip correctly on click', async () => {
+    const element = await fixture(html`
+      <kemet-popover fire-on-click></kemet-popover>
+    `);
+
+    element.shadowRoot.getElementById('trigger').click();
+
+    expect(element.opened).to.equal(true);
   });
 
   it('passes the a11y audit', async () => {
-    const el = await fixture(html`
+    const element = await fixture(html`
       <kemet-popover></kemet-popover>
     `);
 
-    await expect(el).shadowDom.to.be.accessible();
+    await expect(element).shadowDom.to.be.accessible();
   });
 });
